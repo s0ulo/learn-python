e4c604aca99be40270e1d4bd9406c24ec10593db
Dee Nguyen <pinojke@gmail.com>
2020-09-08 22:04:37 +0300

    Refactor excercise 2

--- projects/homework 1/2_if2.py
+++ projects/homework 1/2_if2.py
@@ -18,26 +18,26 @@
 def main(str1, str2):
 	if type(str1) != str or type(str2) != str:
 		return  0
-	if str1 == str2:
-		return 1
 	
 	if str1 != str2:
 		if len(str1) > len(str2) and str2 == 'learn':
-			return 'Могу вернуть и 2, и 3. Выбирай что хочешь, дорогой!'
+			return 'Могу вернуть и 2, и 3.'
 		if str1 != str2 and len(str1) > len(str2):
 			return 2
 		elif str1 != str2 and str2 == 'learn':
 			return 3
-	
+		else:
+			return 'В задаче не предусмотрен этот случай'
 	else:
-		return 'В задаче не предусмотрен этот случай'
+		return 1
 		
 """ 
 Что должна возвращать функция, если строки разные, первая длиннее, а вторая 'learn'?
 """
 
 if __name__ == "__main__":
-	print(main(3.5, 'str'))
+	print(main(3.5, 1))
+	print(main('3', 3))
 	print(main('string', []))
 	print(main('laarn', 'learn'))
 	print(main('learn', 'learn'))
