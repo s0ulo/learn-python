40893061d57563ef51b3121d8ecfbfd75f42e190
Dee Nguyen <pinojke@gmail.com>
2020-09-13 23:02:31 +0300

    Fix exc3

--- projects/homework 1/3_for.py
+++ projects/homework 1/3_for.py
@@ -11,8 +11,10 @@
 """
 
 def get_avg_scores(lst):
+	all_scores = []
 	for dct in lst:
-		all_scores = [score for score in dct['scores']]
+		all_scores.extend(dct['scores'])
+
 	print(f'The average school score is {round((sum(all_scores) / len(all_scores)),2)}')
 	return sum(all_scores) / len(all_scores)
 	
@@ -23,7 +25,13 @@ def get_class_avg(lst):
 		print(dct['school_class'] + ' : ' + str(round(avg_score, 2)))
 
 def main():
-	school = [{'school_class': '4a', 'scores': [3,4,4,5,2]}, {'school_class': '5b', 'scores': [2,3,4,5,2,5]}, {'school_class': '6c', 'scores': [4,5,4,5,3,4,2]}, {'school_class': '7a', 'scores': [3,3,2,3,4,4,5,2]}, {'school_class': '8c', 'scores': [3,3,3,5,2,2,5]}, {'school_class': '7b', 'scores': [5,5,5,5,3,4,4]}]
+	school = [
+		{'school_class': '4a', 'scores': [3,4,4,5,2,5,4]},
+		{'school_class': '5b', 'scores': [2,3,4,5,3,5]},
+		{'school_class': '6c', 'scores': [4,5,4,5,3,4,2]},
+		{'school_class': '7a', 'scores': [3,3,5,3,4,4,5,2]},
+		{'school_class': '8c', 'scores': [3,3,3,5,2,2,5]},
+		{'school_class': '7b', 'scores': [5,5,5,5,3,4,4]}]
 
 	get_avg_scores(school)
 	get_class_avg(school)
