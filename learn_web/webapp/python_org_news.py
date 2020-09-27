89f392efff8511491a4587ae1e54a7fd9f434788
Dee Nguyen <pinojke@gmail.com>
2020-09-27 03:40:45 +0300

    Add database support

--- learn_web/webapp/python_org_news.py
+++ learn_web/webapp/python_org_news.py
@@ -1,34 +1,47 @@
+from datetime import datetime
+
 import requests
 from bs4 import BeautifulSoup
+from webapp.model import db, News
+
+
+def get_html(url):  # scrape html code from url
+    try:
+        result = requests.get(url)  # loal html into result
+        result.raise_for_status()
+        return result.text  # convert html to text for further analysis
+    except(requests.RexuestException, ValueError):
+        return False
 
 
 def get_python_news():
     html = get_html('https://www.python.org/blogs/')  # get text html from url
     if html:
-        # create parsed object version?
+        # create parsed (searchable) object version?
         soup = BeautifulSoup(html, 'html.parser')
 
         # find all embedded <li> in -> exact <ul> named 'list-recent-posts'
         news_list = soup.find('ul', class_='list-recent-posts').findAll('li')
 
-        result_news = []  # list of dicts with news from every <li>
+        # result_news = []  # list of dicts with news from every <li>
         for news in news_list:
             title = news.find('a').text  # get news title from <a>
             url = news.find('a')['href']  # note the ['href'] cuz it's dict obj
             published = news.find('time').text  # get time from <time>
-            result_news.append({
-                'title': title,
-                'url': url,
-                'published': published
-            })
-        return result_news
-    return False
+            try:
+                published = datetime.strptime(published, '%Y-%m-%d')
+            except(ValueError):
+                published = datetime.now()
+            save_news(title, url, published)
 
 
-def get_html(url):
-    try:
-        result = requests.get(url)  # loal html entirely from url
-        result.raise_for_status()
-        return result.text  # text represent html
-    except(requests.RexuestException, ValueError):
-        return False
+def save_news(title, url, published):  # export news object to database
+
+    # Integrity test (search for dubs)
+    news_exists = News.query.filter(News.url == url).count()
+    print(news_exists)  # show how many dubs found
+
+    if not news_exists:
+        new_news = News(title=title, url=url, published=published)
+        db.session.add(new_news)  # add object to database session in Alchemy
+        db.session.commit()  # commit (record) object to database
