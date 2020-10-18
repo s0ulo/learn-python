01403cf58330cf78de398a988ff9d8fab210e14a
Dee Nguyen <pinojke@gmail.com>
2020-10-18 12:12:16 +0300

    Add run shell script and flask-migrate

--- learn_web/webapp/__init__.py
+++ learn_web/webapp/__init__.py
@@ -1,6 +1,7 @@
 # flask.flash - adds ability to pass messages between routes
 from flask import Flask
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 from webapp.db import db
 from webapp.admin.views import blueprint as admin_blueprint
@@ -13,6 +14,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile("config.py")
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     login_manager = LoginManager()
     login_manager.init_app(app)
